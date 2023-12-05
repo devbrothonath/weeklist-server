fbd4bbf456b863952d49a341bbf6900b48638612
Devbrotho Nath <devbrotho.dn@gmail.com>
2023-12-05 20:25:56 +0530

    week list routes

--- routes/weeklistRoutes.js
+++ routes/weeklistRoutes.js
@@ -1,3 +1,8 @@
 const { Router } = require("express");
 
 const router = Router();
+
+router.post("/add-week-list");
+router.put("/update-week-lists")
+router.put("/mark-unmark-tasks")
+router.get("/get-all-week-lists")
